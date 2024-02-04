ab8d1a146d6c79d9e9dfcd562332336572912d90
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-02-04 16:23:13 +0100

    up fix

--- src/proyects.js
+++ src/proyects.js
@@ -2,6 +2,7 @@ import js from "../public/assets/img/fondo_1.jpg"
 import react from "../public/assets/img/fondo_1.jpg"
 import mysql from "../public/assets/img/fondo_1.jpg"
 
+
 export const proyects = [
   {
     id: 1,
