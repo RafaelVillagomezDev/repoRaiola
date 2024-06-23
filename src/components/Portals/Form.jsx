ff63dbcfb70e32833053c974f4450c9a86b1df11
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-06-23 20:03:20 +0200

    Add seet alert

--- src/components/Portals/Form.jsx
+++ src/components/Portals/Form.jsx
@@ -14,38 +14,51 @@ import { createPortal } from "react-dom";
 import { FaX } from "react-icons/fa6";
 import { sendEmail } from "../../services/sendMail";
 import { generateToken } from "../../services/generateToken";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 function Form(props) {
   const portalForm = document.body;
-  
+
   const [formData, setFormData] = useState({
-    email: '',
-    subject: '',
-    messague: '',
+    email: "",
+    subject: "",
+    messague: "",
   });
+
+  const MySwal = withReactContent(Swal);
 
   // Función para manejar cambios en los campos del formulario
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    setFormData((prevValues)=>({
+    setFormData((prevValues) => ({
       ...prevValues,
-      [name]: value
+      [name]: value,
     }));
   };
 
-  const submitEmail=async (event)=>{
+  const submitEmail = async (event) => {
     event.preventDefault();
-    
+
     try {
-      const token=await generateToken(formData);
+      const token = await generateToken(formData);
       const response = await sendEmail(token);
-      console.log('Response:', response);
+     
+      MySwal.fire({
+        icon: "success",
+        title: "Gracias!",
+        text: response.messague,
+      })
     } catch (error) {
-      console.error('Error:', error);
+      MySwal.fire({
+        icon: "error",
+        title: "Error",
+        text: error,
+      })
+      console.error("Error:", error);
     }
-  
-  }
+  };
 
   return createPortal(
     <ContainerForm openPortal={props.openPortal ? props.openPortal : undefined}>
@@ -54,21 +67,38 @@ function Form(props) {
           <FaX style={{ color: "#FFF" }} size={"18px"} />
         </BtnClosePortal>
         <FormPortal>
-            <BoxInput>
-              <LabelInput htmlFor="email">Email</LabelInput>
-              <InputPortal  type="text" name="email" value={formData.email}  onChange={handleChange}  id="email" />
-            </BoxInput>
-            <BoxInput>
-              <LabelInput  htmlFor="asunto">Asunto</LabelInput>
-              <InputPortal  id="subject" name="subject" type="text"   value={formData.subject} onChange={handleChange}/>
-            </BoxInput>
-            <BoxInput>
-              <LabelInput htmlFor="mensaje" >Mensaje</LabelInput>
-              <InputArea name="messague" id="messague" value={formData.messague} onChange={handleChange} />
-            </BoxInput>
-            <BoxInput>
-              <InputSubmit  type="submit" onClick={submitEmail} value={"Enviar"}/>
-            </BoxInput>
+          <BoxInput>
+            <LabelInput htmlFor="email">Email</LabelInput>
+            <InputPortal
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              id="email"
+            />
+          </BoxInput>
+          <BoxInput>
+            <LabelInput htmlFor="asunto">Asunto</LabelInput>
+            <InputPortal
+              id="subject"
+              name="subject"
+              type="text"
+              value={formData.subject}
+              onChange={handleChange}
+            />
+          </BoxInput>
+          <BoxInput>
+            <LabelInput htmlFor="mensaje">Mensaje</LabelInput>
+            <InputArea
+              name="messague"
+              id="messague"
+              value={formData.messague}
+              onChange={handleChange}
+            />
+          </BoxInput>
+          <BoxInput>
+            <InputSubmit type="submit" onClick={submitEmail} value={"Enviar"} />
+          </BoxInput>
         </FormPortal>
       </BoxPortal>
     </ContainerForm>,
